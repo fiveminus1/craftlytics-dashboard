a6b6453ccaccb2ecb03f6e5723c9051d77db0173
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2025-01-03 23:51:24 -0800

    componentized navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 import './App.css';
 import './index.css';
 import PlayerData from './components/PlayerData';
@@ -7,46 +6,21 @@ import PlayerKills from './components/PlayerKills';
 import BiomesExplored from './components/BiomesExplored';
 import PlayerHead from './components/PlayerHead';
 import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar';
 
 function App() {
   const [playerUuid, setPlayerUuid] = useState(null);
-  const [username, setUsername] = useState('');
   const [searchUsername, setSearchUsername] = useState('');
 
-  const handleSearch = (e) => {
-    e.preventDefault();
+  const handleSearch = (username) => {
     setSearchUsername(username);
   }
 
   return (
     <div className="flex flex-col bg-custom-black min-h-screen font-hubot">
       {/* Navbar */}
-      <nav className="bg-custom-dark-red p-4 flex justify-between items-center">
-        <div className="flex items-center space-x-4">
-         <h1 className="text-custom-white text-2x1 font-hubot font-bold">Craftlytics</h1>
-          <button className="bg-custom-red text-custom-white px-4 py-2 rounded-lg hover:bg-custom-dark-red transition-colors">
-            Home
-          </button>
-        </div>
-
-        
-        <form onSubmit={handleSearch} className="relative flex items-center">
-          <input 
-              type="text"
-              placeholder="Enter Username"
-              className="pl-4 pr-10 py-2 border-2 rounded-lg focus:outline-none bg-transparent text-custom-white placeholder-custom-white 
-              placeholder-opacity-75 transition-all hover:border-gradient focus:border-gradient"
-              value={username}
-              onChange = {(e) => setUsername(e.target.value)}
-            /> 
-            <button 
-              type="submit"
-              className="absolute right-2 p-1 text-custom-gray hover:bg-custom-red transition-colors"
-            >
-              <MagnifyingGlassIcon className="h-5 w-5" />
-            </button>
-        </form>
-      </nav>
+      <Navbar onSearch={handleSearch} />
+      
 
       <div className="flex flex-grow">
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+
+const Navbar = ({onSearch}) => {
+    const [username, setUsername] = React.useState('');
+    
+    const handleSearch = (e) => {
+        e.preventDefault();
+        onSearch(username);
+    }
+
+
+    return (
+        <nav className="bg-custom-dark-red p-4 flex justify-between items-center">
+            <div className="flex items-center space-x-4">
+                <h1 className="text-custom-white text-2x1 font-hubot font-bold">Craftlytics</h1>
+                <button className="bg-custom-red text-custom-white px-4 py-2 rounded-lg hover:bg-custom-dark-red transition-colors">
+                    Home
+                </button>
+            </div>
+
+            <form onSubmit={handleSearch} className="relative flex items-center">
+            <input 
+                type="text"
+                placeholder="Enter Username"
+                className="pl-4 pr-10 py-2 border-2 rounded-lg focus:outline-none bg-transparent text-custom-white placeholder-custom-white 
+                placeholder-opacity-75 transition-all hover:border-gradient focus:border-gradient"
+                value={username}
+                onChange = {(e) => setUsername(e.target.value)}
+            /> 
+            <button 
+                type="submit"
+                className="absolute right-2 p-1 text-custom-gray hover:bg-custom-red transition-colors"
+            >
+                <MagnifyingGlassIcon className="h-5 w-5" />
+            </button>
+            </form>
+        </nav>
+    );
+};
+
+export default Navbar;
